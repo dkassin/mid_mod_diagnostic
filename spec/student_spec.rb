9ea7e0ba193f6a52f6348819ceb18d545c17b18c
David Kassin <david.j.kassin@gmail.com>
2021-10-27 10:14:47 -0600

    test: Show the log_score method adds scores to @score variable

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -6,14 +6,27 @@ describe Student do
     expect(student).to be_a Student
   end
 
-  it 'Can show a students name' do
+  it "Can show a student's name" do
     student = Student.new({name: "Morgan", age: 21})
     expect(student.name).to eq ("Morgan")
   end
 
-  it 'Can show a students age' do
+  it "Can show a student's age do" do
     student = Student.new({name: "Morgan", age: 21})
     expect(student.age).to eq (21)
   end
+
+  it "Can show a student's score" do
+    student = Student.new({name: "Morgan", age: 21})
+    expect(student.score).to eq ([])
+  end
+
+  it "Can log a students score with log_score method" do
+    student = Student.new({name: "Morgan", age: 21})
+    student.log_score(89)
+    student.log_score(78)
+    expect(student.score).to eq ([89,78])
+  end
+  
 
 end
