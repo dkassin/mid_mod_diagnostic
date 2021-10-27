298ff8547612551d16b1fc3199c2a21dbb3d04a6
David Kassin <david.j.kassin@gmail.com>
2021-10-27 10:37:24 -0600

    feat: Add full? method

--- lib/course.rb
+++ lib/course.rb
@@ -1,6 +1,18 @@
 require './lib/student'
 
 class Course
+
+  attr_reader :name,
+              :capacity,
+              :students
+
   def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
+
+  def full?
+    @students.count > @capacity
   end
 end
