c641bf6f11e824cfc980e8651055e25097925842
David Kassin <david.j.kassin@gmail.com>
2021-10-27 10:22:02 -0600

    test: Show grade method can return the average of @scores elements

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -18,15 +18,21 @@ describe Student do
 
   it "Can show a student's score" do
     student = Student.new({name: "Morgan", age: 21})
-    expect(student.score).to eq ([])
+    expect(student.scores).to eq ([])
   end
 
   it "Can log a students score with log_score method" do
     student = Student.new({name: "Morgan", age: 21})
     student.log_score(89)
     student.log_score(78)
-    expect(student.score).to eq ([89,78])
+    expect(student.scores).to eq ([89,78])
   end
-  
+
+  it "Show average of all elements in score variable" do
+    student = Student.new({name: "Morgan", age: 21})
+    student.log_score(89)
+    student.log_score(78)
+    expect(student.grade).to eq (83.5)
+  end
 
 end
