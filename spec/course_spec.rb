859215275c6edd2a684392bec9c2d4ca5a7f7e77
David Kassin <david.j.kassin@gmail.com>
2021-10-27 10:37:08 -0600

    test: Return true/false if class if full

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -6,4 +6,24 @@ describe Course do
     course = Course.new("Calculus", 2)
     expect(course).to be_a Course
   end
+
+  it 'Can show course name' do
+    course = Course.new("Calculus", 2)
+    expect(course.name).to eq ("Calculus")
+  end
+
+  it 'Can show course capacity' do
+    course = Course.new("Calculus", 2)
+    expect(course.capacity).to eq (2)
+  end
+
+  it 'Can show students in class' do
+    course = Course.new("Calculus", 2)
+    expect(course.students).to eq ([])
+  end
+
+  it 'Can show if a class is full' do
+    course = Course.new("Calculus", 2)
+    expect(course.full?).to eq (false)
+  end
 end
